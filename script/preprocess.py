483e3958428c9ed4bd1b552ce12e705757d3ff8d
houzeyu2683 <houzeyu2683@gmail.com>
2022-03-21 01:31:33 +0800

    ok

--- script/preprocess.py
+++ script/preprocess.py
@@ -41,7 +41,7 @@ cache.article = library.pandas.concat([default, cache.article])
 cache.save(what=cache.article, file='article.csv', format='csv')
 
 ##  針對 transaction 表進行前處理, 以用戶當作 row 來建構對應的標記與特徵序列.
-table.transaction = table.transaction.head(200000)
+table.transaction = table.transaction
 table.transaction['sales_channel_id'] = feature.category.encode(table.transaction['sales_channel_id']) + 3
 table.transaction['price'] = 1 + (table.transaction['price'] / table.transaction['price'].max())
 selection = ['article_code'] + [
@@ -98,46 +98,46 @@ cache.f1['seq_len'] = cache.f1['article_code'].apply(lambda x: len(x.split()))
 cache.save(cache.f1, 'f1.csv', 'csv')
 
 ##  整合類別種類.
-cache.embedding = dict()
-loop = ['postal_code'] + ['article_code'] +[
-    'product_code', 'prod_name', 
-    'product_type_no', 'product_type_name',
-    'product_group_name', 'graphical_appearance_no',
-    'graphical_appearance_name', 'colour_group_code', 
-    'colour_group_name', 'perceived_colour_value_id', 
-    'perceived_colour_value_name', 'perceived_colour_master_id', 
-    'perceived_colour_master_name', 'department_no', 
-    'department_name', 'index_code', 
-    'index_name', 'index_group_no', 
-    'index_group_name', 'section_no', 
-    'section_name', 'garment_group_no', 
-    'garment_group_name', 'detail_desc'
-] + ['sales_channel_id']
-for l in loop:
+# cache.embedding = dict()
+# loop = ['postal_code'] + ['article_code'] +[
+#     'product_code', 'prod_name', 
+#     'product_type_no', 'product_type_name',
+#     'product_group_name', 'graphical_appearance_no',
+#     'graphical_appearance_name', 'colour_group_code', 
+#     'colour_group_name', 'perceived_colour_value_id', 
+#     'perceived_colour_value_name', 'perceived_colour_master_id', 
+#     'perceived_colour_master_name', 'department_no', 
+#     'department_name', 'index_code', 
+#     'index_name', 'index_group_no', 
+#     'index_group_name', 'section_no', 
+#     'section_name', 'garment_group_no', 
+#     'garment_group_name', 'detail_desc'
+# ] + ['sales_channel_id']
+# for l in loop:
 
-    if(l=="postal_code"):
+#     if(l=="postal_code"):
 
-        v = cache.customer[l].astype(int)
-        pass
+#         v = cache.customer[l].astype(int)
+#         pass
 
-    elif(l=="sales_channel_id"):
+#     elif(l=="sales_channel_id"):
 
-        v = [0,1,2] + [int(j) for i in cache.f1['sales_channel_id'] for j in i.split()]
-        v = library.pandas.Series(v)
-        pass
+#         v = [0,1,2] + [int(j) for i in cache.f1['sales_channel_id'] for j in i.split()]
+#         v = library.pandas.Series(v)
+#         pass
 
-    else:
+#     else:
 
-        v = cache.article[l].astype(int)
-        pass
+#         v = cache.article[l].astype(int)
+#         pass
     
-    s = (
-        min(v),
-        max(v), 
-        v.nunique()
-    )
-    cache.embedding[l] = ["(min:{},max:{},unique:{})".format(s[0], s[1], s[2])]
-    pass
+#     s = (
+#         min(v),
+#         max(v), 
+#         v.nunique()
+#     )
+#     cache.embedding[l] = ["(min:{},max:{},unique:{})".format(s[0], s[1], s[2])]
+#     pass
 
-cache.embedding = library.pandas.DataFrame(cache.embedding)
-cache.save(cache.embedding, 'embedding.csv', 'csv')
+# cache.embedding = library.pandas.DataFrame(cache.embedding)
+# cache.save(cache.embedding, 'embedding.csv', 'csv')
